5f3f2daf45f324a225529d3838b69f0f6f3fd209
draeder <draeder@gmail.com>
2021-02-13 00:37:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,4 @@
     // Create a web sockets signaling server
-    var express = require('express');
-    var bodyParser = require('body-parser');
-    var app = express();
-
-    //Allow all requests from all domains & localhost
-    /*app.all('/*', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Methods", "POST, GET");
-        next();
-    });
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: false}));*/
-
     let lookup = {}
 
     const hostname = '0.0.0.0';
@@ -59,11 +44,3 @@
 
     server.on('complete', function (addr) {})
     server.on('stop', function (addr) {})
-
-    /*app.get('/peers', function(req, res) {
-        res.send(lookup);
-    })
-
-    app.listen(3001,function(){
-        console.log('Express server port: ' + this.address().port); //Listening on port #
-    })*/
