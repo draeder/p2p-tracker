a4752ae3adf2bd96903160f47f3e0f7e8a28d8a6
draeder <draeder@gmail.com>
2021-02-12 23:28:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,6 @@
         res.send(lookup);
     });
 
-    app.listen(3001 || process.env.PORT,function(){
+    app.listen(process.env.PORT + 1,function(){
         console.log('Express server port: ' + this.address().port); //Listening on port 8888
     });
