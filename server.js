3381fb88b94e5f50fd9c15e3744499469857ef46
draeder <draeder@gmail.com>
2022-04-10 10:34:39 -0500

    Heroku SERVER_URL env var test

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
-    // Create a web sockets signaling server
-    let lookup = {}
+require('dotenv').config()
+console.log('Heroku server URL:', `https://${process.env.SERVER_URL}.herokuapp.com/`)
+// Create a web sockets signaling server
+let lookup = {}
 
-    const hostname = '0.0.0.0';
-    const port = process.env.PORT || 3001;
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3001;
 
 
-    var Server = require('bittorrent-tracker').Server
+const Server = require('bittorrent-tracker').Server
 
-    var server = new Server({
-        udp: false, // enable udp server? [default=true]
-        http: true, // enable http server? [default=true]
-        ws: true, // enable websocket server? [default=true]
-        stats: true, // enable web-based statistics? [default=true]
-    })
+let server = new Server({
+  udp: false, // enable udp server? [default=true]
+  http: true, // enable http server? [default=true]
+  ws: true, // enable websocket server? [default=true]
+  stats: true, // enable web-based statistics? [default=true]
+})
 
-    server.on('error', function (err) {
-        // fatal server error!
-        //console.log(err.message)
-    })
+server.on('error', function (err) {
+  // fatal server error!
+  //console.log(err.message)
+})
 
-    server.on('warning', function (err) {
-        // client sent bad data. probably not a problem, just a buggy client.
-        //console.log(err.message)
-    })
+server.on('warning', function (err) {
+  // client sent bad data. probably not a problem, just a buggy client.
+  //console.log(err.message)
+})
 
-    server.on('listening', function () {
-        // fired when all requested servers are listening
-        console.log('Signal server http port:' + server.http.address().port)
-        console.log('Signal server ws port:' + server.ws.address().port)
-    })
+server.on('listening', function () {
+  // fired when all requested servers are listening
+  console.log('Signal server http port:' + server.http.address().port)
+  console.log('Signal server ws port:' + server.ws.address().port)
+})
 
-    // listen for individual tracker messages from peers:
-    server.on('start', function (addr) {
-        console.log('got start message from ' + addr)
-        Object.keys(server.torrents).forEach(hash => {
-            lookup[server.torrents[hash].infoHash] = server.torrents[hash].peers.length
-            //console.log("peers: " + server.torrents[hash].peers.length)
-        })
-    })
+// listen for individual tracker messages from peers:
+server.on('start', function (addr) {
+  console.log('got start message from ' + addr)
+  Object.keys(server.torrents).forEach(hash => {
+    lookup[server.torrents[hash].infoHash] = server.torrents[hash].peers.length
+    //console.log("peers: " + server.torrents[hash].peers.length)
+  })
+})
 
-    server.on('complete', function (addr) {})
-    server.on('stop', function (addr) {})
+server.on('complete', function (addr) {})
+server.on('stop', function (addr) {})
 
-    // start tracker server listening! Use 0 to listen on a random free port.
-    server.listen(port, hostname, 'listening')
+// start tracker server listening! Use 0 to listen on a random free port.
+server.listen(port, hostname, 'listening')
